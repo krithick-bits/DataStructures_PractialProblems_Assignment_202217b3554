230f62112fce7e8a1e64f90fa80415183c170081
krithick-bits <202217b3554@wilp.bits-pilani.ac.in>
2024-07-21 21:48:16 +0530

    Add files via upload

---
+++ Problem8_202217b3554/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+void selection_sort(int arr[], int n)
+{
+    int i, j, min_idx;
+    for (i = 0; i < n - 1; i++)
+    {
+        min_idx = i;
+        for (j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_idx])
+            {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        if (min_idx != i)
+        {
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+}
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    selection_sort(arr, n);
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
